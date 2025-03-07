681c29ab39dd290db74b77a6ab7cab35626d59b7
LeyLopez <leidislopez2017@gmail.com>
2025-03-06 19:01:21 -0500

    Repositories

---
+++ src/main/java/com/tournaments/tournaments/repositories/TournamentRepository.java
@@ -0,0 +1,9 @@
+package com.tournaments.tournaments.repositories;
+
+import com.tournaments.tournaments.entities.Tournament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Integer> {
+}
