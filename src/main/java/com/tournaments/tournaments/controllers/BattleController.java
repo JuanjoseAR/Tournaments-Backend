f1554fe81007b19dd9f7b6d3c17b22c3d2b807f5
diomedescerda <kenmatwofour@gmail.com>
2025-03-09 17:31:12 -0500

    feat(controllers): stuffy controllers

---
+++ src/main/java/com/tournaments/tournaments/controllers/BattleController.java
@@ -0,0 +1,27 @@
+package com.tournaments.tournaments.controllers;
+
+import com.tournaments.tournaments.dto.BattleDTO;
+import com.tournaments.tournaments.services.BattleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tournament/matches")
+
+public class BattleController {
+    private final BattleService battleService;
+
+    public BattleController(BattleService battleService) {
+        this.battleService = battleService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BattleDTO> getMatchups(@PathVariable("id") Integer id) {
+        return battleService.getMatchupsById(id); //waiting44444
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<BattleDTO> createMatchups(@PathVariable("id") Integer id) {
+        return battleService.createMatchupsById(id); //same
+    }
+}
--- src/main/java/com/tournaments/tournaments/controllers/TournamentStateController.java
+++ src/main/java/com/tournaments/tournaments/controllers/TournamentStateController.java
@@ -1,4 +1,33 @@
 package com.tournaments.tournaments.controllers;
 
+import com.tournaments.tournaments.dto.TournamentStateDTO;
+import com.tournaments.tournaments.entities.TournamentState;
+import com.tournaments.tournaments.services.TournamentStateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tournament/status")
+
 public class TournamentStateController {
+    private final TournamentStateService tournamentStateService;
+
+    public TournamentStateController(TournamentStateService tournamentStateService) {
+        this.tournamentStateService = tournamentStateService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TournamentStateDTO> createTournamentState(@RequestBody TournamentStateDTO tournamentState) {
+        return tournamentStateService.createTournamentState(tournamentState); // change to accept DTO
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TournamentStateDTO> updateTournamentState(@PathVariable("id") Integer id, @RequestBody TournamentStateDTO tournamentState) {
+        Optional<TournamentStateDTO> tournamentStateUpdate = tournamentStateService.updateTournamentStateById(id, tournamentState); //update the service to accept DTO
+        return tournamentStateUpdate.map(a -> ResponseEntity.ok(a)).orElseGet(() -> {
+            return createTournamentState(tournamentState);
+        });
+    }
 }
