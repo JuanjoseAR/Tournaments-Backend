3f94da75ca5595241a2d2afb92dd0ba98b5bad61
JuanjoseAR <jarango@unimagdalena.edu.co>
2025-03-25 21:02:17 -0500

    minor fix5

--- src/main/java/com/tournaments/tournaments/controllers/TournamentRegistrationController.java
+++ src/main/java/com/tournaments/tournaments/controllers/TournamentRegistrationController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/tournament/register")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins =  "*")
 public class TournamentRegistrationController {
     private final TournamentRegistrationService tournamentRegistrationService;
 
